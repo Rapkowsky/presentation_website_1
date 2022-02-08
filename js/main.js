58f09da088d08a1d9e0f5db0024f78d125130df5
Rapkowsky <robertrapkowski19@gmail.com>
2022-02-08 22:03:04 +0100

    JS nav bars color changed added

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 const nav = document.querySelector(".nav");
-const navBtn = document.querySelector(".burger-btn");
+const navButton = document.querySelector(".burger-btn");
 const allNavItems = document.querySelectorAll(".nav__item");
+const navButtonBars = document.querySelector(".burger-btn__bars");
+const allSections = document.querySelectorAll(".section");
+const footerYear = document.querySelector(".footer__year");
 
 const handleNav = () => {
 	nav.classList.toggle("nav--active");
+
+	navButtonBars.classList.remove("black-bars-color");
 
 	allNavItems.forEach((item) => {
 		item.addEventListener("click", () => {
@@ -23,4 +28,24 @@ const handleNavItemsAnimation = () => {
 	});
 };
 
-navBtn.addEventListener("click", handleNav);
+const handleTracking = () => {
+	const currentSection = window.scrollY;
+
+	allSections.forEach((section) => {
+		// offsetTop - we checked if top border of our section is <= currentSection. 60 pixels is the reserve for indentation (white block elements)
+		if (section.classList.contains("white-section") && section.offsetTop <= currentSection + 60) {
+			navButtonBars.classList.add("black-bars-color");
+		} else if (!section.classList.contains("white-section") && section.offsetTop <= currentSection + 60) {
+			navButtonBars.classList.remove("black-bars-color");
+		}
+	});
+};
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+
+handleCurrentYear();
+navButton.addEventListener("click", handleNav);
+window.addEventListener("scroll", handleTracking);
